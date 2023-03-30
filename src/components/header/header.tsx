2a6f36f9c7fda90d21bff076db49d8eb37d0487d
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2023-03-30 19:52:28 +0530

    Update header.tsx

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,5 +1,4 @@
 import Toggle from '../shared/toggle'
-import { Link } from '@builder.io/qwik-city'
 import { component$ } from '@builder.io/qwik'
 
 export default component$(() => {
@@ -8,21 +7,21 @@ export default component$(() => {
       <div class="flex w-full max-w-[90vw] flex-row items-center justify-between sm:px-10 lg:max-w-[75vw]">
         <Toggle />
         <div class="relative flex max-w-[258px] flex-row items-center space-x-5 overflow-x-scroll sm:max-w-none sm:overflow-x-hidden">
-          <Link class="dark:text-white" href="/">
+          <a class="dark:text-white" href="/">
             Home
-          </Link>
-          <Link class="dark:text-white" href="/about">
+          </a>
+          <a class="dark:text-white" href="/about">
             About
-          </Link>
-          <Link class="dark:text-white" href="/blogs">
+          </a>
+          <a class="dark:text-white" href="/blogs">
             Blogs
-          </Link>
-          <Link class="dark:text-white" href="/cv">
+          </a>
+          <a class="dark:text-white" href="/cv">
             CV
-          </Link>
-          <Link class="dark:text-white" href="/storyblok">
+          </a>
+          <a class="dark:text-white" href="/storyblok">
             Storyblok
-          </Link>
+          </a>
         </div>
       </div>
     </nav>
